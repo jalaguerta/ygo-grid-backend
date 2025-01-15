5fe2a908f3be4839f8b8f46c527ee8757cb003a0
jalaguerta <jalaguerta@gmail.com>
2025-01-15 23:45:43 +0900

    added autocomplete url

--- grid/urls.py
+++ grid/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import test_view, ValidateCardAPIView
+from .views import test_view, autocomplete_cards, ValidateCardAPIView
 
 urlpatterns = [
-    path('validate-card/', ValidateCardAPIView.as_view(), name='validate_card'),
     path('test-view/', test_view, name='test_view'),
+    path('autocomplete/', autocomplete_cards, name='autocomplete_cards'),
+    path('validate-card/', ValidateCardAPIView.as_view(), name='validate_card'),
 ]
